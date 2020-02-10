08e0c798fdbe69562a88a6470a5a93a91311602f
ItsKamsDude <adithyakamath96@gmail.com>
2020-02-11 03:04:12 +0530

    Initial bot release

---
+++ app.py
@@ -0,0 +1,61 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys 
+import time 
+
+class InstagramBot:
+    
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
+        self.driver = webdriver.Firefox()
+
+    def closeBrowser(self):
+        self.driver.close() 
+
+    def login(self):
+        driver = self.driver
+        driver.get("https://www.instagram.com/")
+        time.sleep(5)
+        login_button = driver.find_element_by_xpath("//a[@href='/accounts/login/?source=auth_switcher']")
+        login_button.click()
+        time.sleep(5)
+        user_name_elem = driver.find_element_by_xpath("//input[@name='username']")
+        user_name_elem.clear()
+        user_name_elem.send_keys(self.username)
+        password_elem = driver.find_element_by_xpath("//input[@name='password']")
+        password_elem.clear()
+        password_elem.send_keys(self.password)
+        password_elem.send_keys(Keys.RETURN)
+        
+    def like_photo(self,hashtag):
+        driver = self.driver
+        # fck_notifications = driver.find_element_by_class_name("aOOlW   HoLwm ")
+        # fck_notifications.click()
+        time.sleep(5)
+        driver.get("https://www.instagram.com/explore/tags/"+hashtag+"/")
+        time.sleep(5)
+        for i in range(1,3):
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(2)
+
+        hrefs = driver.find_elements_by_tag_name('a')
+        pic_hrefs = [elem.get_attribute('href') for elem in hrefs]
+        pic_hrefs = [href for href in pic_hrefs if hashtag in href]
+        print(hashtag + " photos: "+str(len(pic_hrefs)))
+
+        for pic_href in pic_hrefs:
+            driver.get(pic_href)
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            try:
+                driver.find_element_by_link_text("Like").click()
+                time.sleep(18)
+            except Exception as e:
+                time.sleep(5)
+
+        #"//a[@href'accounts/login']"
+        # "//input[@name='username']"
+        # "//input[@name='password']"
+
+kamiIG = InstagramBot("youremailid","yourpass")
+kamiIG.login()
+kamiIG.like_photo('newyork')
